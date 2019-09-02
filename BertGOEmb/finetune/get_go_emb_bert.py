0b98c994b34a6e5e4af3a4ce419829fc128483b2
Dat Duong <datd1988@gmail.com>
2019-09-01 21:46:40 -0700

    Update get_go_emb_bert.py

--- BertGOEmb/finetune/get_go_emb_bert.py
+++ BertGOEmb/finetune/get_go_emb_bert.py
@@ -1,7 +1,7 @@
 
 from argparse import ArgumentParser
 from pathlib import Path
-import os, pickle, sys
+import os, pickle, sys, re
 import torch
 import logging
 import json
@@ -25,21 +25,27 @@ from pytorch_transformers.optimization import AdamW, WarmupLinearSchedule
 ## we used the GO vectors as "word"
 ## we can get the GO emb directly from the BERT model 
 
-output = "" 
-bert_model = "" 
-word_path = ""
+output = "/local/datdb/deepgo/data/BertFineTuneGOEmb768Result" 
+bert_model = "/local/datdb/deepgo/data/BertFineTuneGOEmb768Result/" ## not need the pytorch.bin thingy 
+word_path = "/local/datdb/deepgo/data/BertFineTuneGOEmb768Result/vocab.txt"
 
 model = BertForPreTraining.from_pretrained(bert_model)
-word_emb = model.bert.embeddings.data.numpy() ## extract as np
+word_emb = model.bert.embeddings.word_embeddings.weight.data.numpy() ## extract as np
 
 word_text = pd.read_csv(word_path,header=None,sep="\t") ## read in the actual word, notice, the ordering here matches the exact ordering of @word_emb
 word_text = list (word_text[0])
 
 word_dict = {}
 for index, word in enumerate(word_text): 
-  word_dict[word_text] = word_emb[index]
+  if word in '[PAD]\n[UNK]\n[CLS]\n[SEP]\n[MASK]\n': 
+    continue
+  if 'GO' in word: 
+    word = re.sub('GO',"GO:",word)
+  #
+  word_dict[word] = word_emb[index]
 
 
-pickle.dump( word_dict, open(output,"wb") ) 
+
+pickle.dump( word_dict, open(os.path.join(output,"GOname_vector.pickle"),"wb") ) 
 
 
