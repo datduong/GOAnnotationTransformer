fb1207be26bf73af6258c7f0bde81825dd28c61a
Dat Duong <datd1988@gmail.com>
2019-09-18 04:35:37 -0700

    fix script

--- finetune/run_token_classify.sh
+++ finetune/run_token_classify.sh
@@ -23,7 +23,8 @@ label_2test='/local/datdb/deepgo/data/train/deepgo.mf.csv'
 cd $server/BertGOAnnotation/finetune/
 
 
+# 5040 batches train
 ## continue training 
-CUDA_VISIBLE_DEVICES=7 python3 -u run_token_classify.py --block_size 2048 --mlm --bert_vocab $bert_vocab --train_data_file $train_masklm_data --output_dir $output_dir --num_train_epochs 50 --per_gpu_train_batch_size 4 --per_gpu_eval_batch_size 4 --config_name $config_name --do_train --model_type bert --overwrite_output_dir --save_steps 300 --logging_steps 10 --evaluate_during_training --eval_data_file $eval_masklm_data --label_2test $label_2test --config_override # --no_cuda
+CUDA_VISIBLE_DEVICES=7 python3 -u run_token_classify.py --block_size 2048 --mlm --bert_vocab $bert_vocab --train_data_file $train_masklm_data --output_dir $output_dir --num_train_epochs 50 --per_gpu_train_batch_size 4 --per_gpu_eval_batch_size 4 --config_name $config_name --do_train --model_type bert --overwrite_output_dir --save_steps 2500 --logging_steps 5000 --evaluate_during_training --eval_data_file $eval_masklm_data --label_2test $label_2test --config_override # --no_cuda
 
 
