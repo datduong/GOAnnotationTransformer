cc31f600b8e4d8fc771bfa4772f91a3a297dca75
Dat Duong <datd1988@gmail.com>
2019-10-04 04:22:45 -0700

    add some plot

--- SeeAttention/manual_check_all_att.py
+++ SeeAttention/manual_check_all_att.py
@@ -9,16 +9,16 @@ GO2all = pickle.load(open("GO2GO_attention_O54992_B3PC73.pickle","rb"))
 go_names = pd.read_csv('/u/scratch/d/datduong/deepgo/data/deepgo.mf.csv',header=None)
 go_names = list(go_names[0])
 
-go = '0016410'
+go = '0002039'
 go_index = go_names.index('GO:'+go) # 0002039
 
-# xc = {} 
-# z = 'GO:0019901;GO:0019900;GO:0019899;GO:0002039;GO:0005515;GO:0005488;GO:0004674;GO:0004672;GO:0016773;GO:0016301;GO:0016772;GO:0016740;GO:0003824'.split(';')
-# z = list (set(z))
-# xc['O54992'] = [go_names.index(i) for i in z ]
-# z = 'GO:0004553;GO:0016798;GO:0016787;GO:0003824'.split(';')
-# z = list (set(z))
-# xc['B3PC73'] = [go_names.index(i) for i in z ]
+xc = {} 
+z = 'GO:0019901;GO:0019900;GO:0019899;GO:0002039;GO:0005515;GO:0005488;GO:0004674;GO:0004672;GO:0016773;GO:0016301;GO:0016772;GO:0016740;GO:0003824'.split(';')
+z = list (set(z))
+xc['O54992'] = [go_names.index(i) for i in z ]
+z = 'GO:0004553;GO:0016798;GO:0016787;GO:0003824'.split(';')
+z = list (set(z))
+xc['B3PC73'] = [go_names.index(i) for i in z ]
 
 for p in ["O54992","B3PC73"]: 
   print ('prot {}'.format(p))
@@ -27,14 +27,14 @@ for p in ["O54992","B3PC73"]:
     x = this_obs [ head ][go_index]
     largest_signal_pos = x.argsort()[::-1][:10] ## take the 10 largest signal
     print ('head {}'.format(head))
-    print ( [ go_names[signal_pos] for signal_pos in largest_signal_pos ] ) 
-    print ( [ x[signal_pos] for signal_pos in largest_signal_pos ] ) 
+    # print ( [ go_names[signal_pos] for signal_pos in largest_signal_pos ] ) 
+    # print ( [ x[signal_pos] for signal_pos in largest_signal_pos ] ) 
     # print ( np.quantile ( np.round(x,6), q=[.25,.5,.75,.95] ) )
-    # plt.clf()
-    # plt.plot(np.arange(len(x)), x , '-')
-    # for xc_line in xc[p]: 
-    #   plt.axvline(x=xc_line,color='r')
-    # plt.savefig( 'go2go'+p+"_"+go+"_"+str(head)+'.png' )
+    plt.clf()
+    plt.plot(np.arange(len(x)), x , '-')
+    for xc_line in xc[p]: 
+      plt.axvline(x=xc_line,color='r')
+    plt.savefig( 'go2go'+p+"_"+go+"_"+str(head)+'.png' )
     
 
 
