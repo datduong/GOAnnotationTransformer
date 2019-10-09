d532cbe5b9893cf0e960f196a38d78fe43d85464
Dat Duong <datd1988@gmail.com>
2019-10-09 04:47:12 -0700

    Update plot_each_head.R

--- SeeAttention/plot_each_head.R
+++ SeeAttention/plot_each_head.R
@@ -18,15 +18,21 @@ plot_list = list()
 counter = 1
 for (layer in 0:9){ 
   for (head in 0:1){
-    fin = read.csv( paste0(p , 'layer' , layer, 'head',head,'.csv'), header=F ) 
-    fin = as.matrix(fin)
+    fin = read.csv( paste0(p , 'layer' , layer, 'head',head,'.csv'), header=F )
+    total_len = nrow(fin) 
+    fin = log ( as.matrix(fin) * 1000 )
     ## remove CLS and SEP ?? 
     # heatmap(fin,scale="none",col = coul,Colv=NA,main=paste('Layer',layer,'Head',head))
     fin = melt(fin)
+    total_median = quantile ( fin[,3], 0.5 ) 
+    print (total_median)
+    total_max = max(fin[,3])
     p1 = ggplot(data = fin, aes(x=Var1, y=Var2, fill=value)) + geom_tile() +
+    geom_vline(xintercept=total_len-num_label) +
+    geom_hline(yintercept=total_len-num_label) +
     ggtitle(paste0(p , 'layer' , layer, 'head',head)) +
-    scale_colour_gradient(low = "white", high = "black") +
-    theme(legend.title = element_blank(),axis.title.x=element_blank(),axis.title.y=element_blank(),axis.text.x = element_blank(),axis.text.y = element_blank(),axis.ticks = element_blank()) 
+    scale_fill_gradient2(low = "blue", high = "red") + # midpoint = total_median, limit = c(0,total_max)
+    theme(legend.title = element_blank(),axis.title.x=element_blank(),axis.title.y=element_blank(),axis.text.x = element_blank(),axis.text.y = element_blank()) 
     png (file = paste0(p,'H',head,'L',layer,'.png'),width=16, height=16, units='in', res = 500)
     print (p1)
     dev.off() 
