118fb55ffb6626f6a0f0a9ac20733837ac8e9213
Dat Duong <dat@dats-MacBook-Pro.local>
2019-10-09 17:48:20 -0700

    change plot

--- SeeAttention/plot_each_head.R
+++ SeeAttention/plot_each_head.R
@@ -17,9 +17,10 @@ p = 'O54992' # B3PC73 O54992
 plot_list = list() 
 counter = 1
 for (layer in 0:9){ 
-  for (head in 0:1){
+  for (head in 0:3){
     fin = read.csv( paste0(p , 'layer' , layer, 'head',head,'.csv'), header=F )
     total_len = nrow(fin) 
+    colnames(fin) = 1:nrow(fin)
     fin = log ( as.matrix(fin) * 1000 )
     ## remove CLS and SEP ?? 
     # heatmap(fin,scale="none",col = coul,Colv=NA,main=paste('Layer',layer,'Head',head))
@@ -32,7 +33,7 @@ for (layer in 0:9){
     geom_hline(yintercept=total_len-num_label) +
     ggtitle(paste0(p , 'layer' , layer, 'head',head)) +
     scale_fill_gradient2(low = "blue", high = "red") + # midpoint = total_median, limit = c(0,total_max)
-    theme(legend.title = element_blank(),axis.title.x=element_blank(),axis.title.y=element_blank(),axis.text.x = element_blank(),axis.text.y = element_blank()) 
+    theme(legend.title = element_blank()) #,axis.title.x=element_blank(),axis.title.y=element_blank(),axis.text.x = element_blank(),axis.text.y = element_blank()) 
     png (file = paste0(p,'H',head,'L',layer,'.png'),width=16, height=16, units='in', res = 500)
     print (p1)
     dev.off() 
