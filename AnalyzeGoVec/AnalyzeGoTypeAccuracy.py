a5b41035be97bc314e980a7ce6dc202f864e1cc0
Dat Duong <dat@dats-MacBook-Pro.local>
2019-11-18 18:05:01 -0800

    add for loop

--- AnalyzeGoVec/AnalyzeGoTypeAccuracy.py
+++ AnalyzeGoVec/AnalyzeGoTypeAccuracy.py
@@ -12,43 +12,47 @@ import PosthocCorrect
 ## check accuracy of labels not seen in training.
 ## pure zeroshot approach.
 
-onto = 'bp'
+# onto = 'bp'
 
-label_original = pd.read_csv('/local/datdb/deepgo/data/train/deepgo.'+onto+'.csv',sep="\t",header=None)
-label_original = set(list(label_original[0]))
+for onto in ['cc','mf','bp']: 
 
-label_large = pd.read_csv('/local/datdb/deepgo/dataExpandGoSet/train/deepgo.'+onto+'.csv',sep="\t",header=None)
-label_large = set(list(label_large[0]))
+  print ('\n\ntype {}'.format(onto))
+    
+  label_original = pd.read_csv('/local/datdb/deepgo/data/train/deepgo.'+onto+'.csv',sep="\t",header=None)
+  label_original = set(list(label_original[0]))
 
-label_unseen = sorted ( list ( label_large - label_original ) ) 
-label_large = sorted(label_large) ## by default we sort label for the model
+  label_large = pd.read_csv('/local/datdb/deepgo/dataExpandGoSet/train/deepgo.'+onto+'.csv',sep="\t",header=None)
+  label_large = set(list(label_large[0]))
 
-label_lookup = {value:counter for counter,value in enumerate(label_large)}
-label_unseen_pos = np.array ( [label_lookup[v] for v in label_lookup if v in label_unseen ] ) 
-label_seen_pos = np.array ( [label_lookup[v] for v in label_lookup if v in label_original ] ) 
+  label_unseen = sorted ( list ( label_large - label_original ) ) 
+  label_large = sorted(label_large) ## by default we sort label for the model
 
-## want to compute accuracy on original set of labels, then on unseen labels
-## possible original set prediction will change because we do joint prediction. so attention weight will affect outcome
+  label_lookup = {value:counter for counter,value in enumerate(label_large)}
+  label_unseen_pos = np.array ( [label_lookup[v] for v in label_lookup if v in label_unseen ] ) 
+  label_seen_pos = np.array ( [label_lookup[v] for v in label_lookup if v in label_original ] ) 
 
-prediction_dict = pickle.load(open("/local/datdb/deepgo/data/BertNotFtAARawSeqGO/"+onto+"/fold_1/2embPpiAnnotE256H1L12I512Set0/YesPpiYesTypeScaleFreezeBert12Ep10e10Drop0.1/save_prediction.pickle","rb"))
+  ## want to compute accuracy on original set of labels, then on unseen labels
+  ## possible original set prediction will change because we do joint prediction. so attention weight will affect outcome
 
-print ('\nsize {}\n'.format(prediction_dict['prediction'].shape))
+  prediction_dict = pickle.load(open("/local/datdb/deepgo/data/BertNotFtAARawSeqGO/"+onto+"/fold_1/2embPpiAnnotE256H1L12I512Set0/YesPpiYesTypeScaleFreezeBert12Ep10e10Drop0.1/save_prediction.pickle","rb"))
 
-def eval (in_dict,sub_array=None): 
-  prediction = prediction_dict['prediction']
-  true_label = prediction_dict['true_label']
-  if sub_array is not None: 
-    prediction = prediction [ : , sub_array ] ## obs x label 
-    true_label = true_label [ : , sub_array ]
-  #
-  result = evaluation_metric.all_metrics ( np.round(prediction) , true_label, yhat_raw=prediction, k=[5,10,15,20,25]) 
-  return result
+  print ('\nsize {}\n'.format(prediction_dict['prediction'].shape))
 
-print ('\nwhole')
-evaluation_metric.print_metrics( eval(prediction_dict) )
+  def eval (in_dict,sub_array=None): 
+    prediction = prediction_dict['prediction']
+    true_label = prediction_dict['true_label']
+    if sub_array is not None: 
+      prediction = prediction [ : , sub_array ] ## obs x label 
+      true_label = true_label [ : , sub_array ]
+    #
+    result = evaluation_metric.all_metrics ( np.round(prediction) , true_label, yhat_raw=prediction, k=[5,10,15,20,25]) 
+    return result
 
-print('\noriginal')
-evaluation_metric.print_metrics( eval(prediction_dict, label_seen_pos) )
+  print ('\nwhole')
+  evaluation_metric.print_metrics( eval(prediction_dict) )
 
-print ('\nunseen')
-evaluation_metric.print_metrics( eval(prediction_dict, label_unseen_pos) )
+  print('\noriginal')
+  evaluation_metric.print_metrics( eval(prediction_dict, label_seen_pos) )
+
+  print ('\nunseen')
+  evaluation_metric.print_metrics( eval(prediction_dict, label_unseen_pos) )
