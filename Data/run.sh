098d5281704b3713d4baed6b00dea042bb860d71
Dat Duong <datd1988@gmail.com>
2019-11-22 02:48:41 -0800

    Update run.sh

--- Data/run.sh
+++ Data/run.sh
@@ -7,8 +7,10 @@
 . /u/local/Modules/default/init/modules.sh
 module load python/3.7.2
 
-cd /u/scratch/d/datduong/BertGOAnnotation/Data/
-python3 get_prot_info.py > log.txt
+# cd /u/scratch/d/datduong/BertGOAnnotation/Data/
+# python3 get_prot_info.py > log.txt
 
+cd /u/scratch/d/datduong/BertGOAnnotation/AnalyzeGoVec
+python3 Combine2blast.py > CombineZeroshot2Blast.txt
 
 
