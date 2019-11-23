6ceac1ede0d4823e30d422719bdd5bc7ea9acc22
Dat Duong <datd1988@gmail.com>
2019-11-23 02:13:23 -0800

    script

--- finetune/Script/Classify2embPretrainProtannot.sh
+++ finetune/Script/Classify2embPretrainProtannot.sh
@@ -12,7 +12,7 @@ pretrained_label_path='/local/datdb/deepgo/data/cosine.AveWordClsSep768.Linear25
 
 choice='2embPpiAnnotE256H1L12I512Set0/PpiNoTypeEp10e10Drop0.1' # Lr5e-5 Dr0.2
 model_type='ppi'
-cache_name='PpiNoType'
+cache_name='YesPpiYesType'
 checkpoint=78012 ## 110726
 
 block_size=1792 # mf and cc 1792 but bp has more term  2048
@@ -33,24 +33,24 @@ for ontology in 'bp' ; do
   bert_vocab=$output_dir/'vocabAA.txt'
   config_name=$output_dir/config.json
 
-  aa_type_file='/local/datdb/deepgo/data/train/fold_1/train_'$ontology'_prot_annot_count.pickle'
+  aa_type_file='/local/datdb/deepgo/data/train/fold_1/ProtAnnotTypeTopoData/train_'$ontology'_prot_annot_type_topo_count.pickle'
 
-  train_masklm_data='/local/datdb/deepgo/data/train/fold_1/TokenClassify/TwoEmb/train-'$ontology'-prot-annot.tsv' ## okay to call it as long as it has ppi
-  eval_masklm_data='/local/datdb/deepgo/data/train/fold_1/TokenClassify/TwoEmb/dev-'$ontology'-prot-annot.tsv'
+  train_masklm_data='/local/datdb/deepgo/data/train/fold_1/ProtAnnotTypeTopoData/train-'$ontology'-input.tsv' ## okay to call it as long as it has ppi
+  eval_masklm_data='/local/datdb/deepgo/data/train/fold_1/ProtAnnotTypeTopoData/dev-'$ontology'-input.tsv'
   label_2test='/local/datdb/deepgo/data/train/deepgo.'$ontology'.csv'
 
   cd $server/BertGOAnnotation/finetune/
 
   # continue training use @model_name_or_path and turn off @config_override
-  # CUDA_VISIBLE_DEVICES=5 python3 -u RunTokenClassifyProtData.py --cache_name $cache_name --block_size $block_size --mlm --bert_vocab $bert_vocab --train_data_file $train_masklm_data --output_dir $output_dir --num_train_epochs 100 --per_gpu_train_batch_size 4 --per_gpu_eval_batch_size 6 --config_name $config_name --do_train --model_type $model_type --overwrite_output_dir --save_steps $save_every --logging_steps $save_every --evaluate_during_training --eval_data_file $eval_masklm_data --label_2test $label_2test --learning_rate 0.0001 --seed 2019 --fp16 --config_override > $output_dir/train_point.txt # --no_cuda
+  CUDA_VISIBLE_DEVICES=5 python3 -u RunTokenClassifyProtData.py --cache_name $cache_name --block_size $block_size --mlm --bert_vocab $bert_vocab --train_data_file $train_masklm_data --output_dir $output_dir --num_train_epochs 100 --per_gpu_train_batch_size 4 --per_gpu_eval_batch_size 6 --config_name $config_name --do_train --model_type $model_type --overwrite_output_dir --save_steps $save_every --logging_steps $save_every --evaluate_during_training --eval_data_file $eval_masklm_data --label_2test $label_2test --learning_rate 0.0001 --seed 2019 --fp16 --config_override > $output_dir/train_point.txt # --no_cuda
 
   ## --pretrained_label_path $pretrained_label_path --aa_type_file $aa_type_file --reset_emb_zero
 
-  ## testing phase --pretrained_label_path $pretrained_label_path
-  for test_data in 'test' 'train'; do # 'dev'
-    eval_masklm_data='/local/datdb/deepgo/data/train/fold_1/TokenClassify/TwoEmb/'$test_data'-'$ontology'-prot-annot.tsv'
-    CUDA_VISIBLE_DEVICES=3 python3 -u RunTokenClassifyProtData.py --cache_name $cache_name --block_size $block_size --mlm --bert_vocab $bert_vocab --train_data_file $train_masklm_data --output_dir $output_dir --per_gpu_eval_batch_size 2 --config_name $config_name --do_eval --model_type $model_type --overwrite_output_dir --evaluate_during_training --eval_data_file $eval_masklm_data --label_2test $label_2test --config_override --eval_all_checkpoints --checkpoint $checkpoint > $output_dir/'eval_'$test_data'_check_point_max.txt'
-  done  # --pretrained_label_path $pretrained_label_path --aa_type_file $aa_type_file --reset_emb_zero
+  # ## testing phase --pretrained_label_path $pretrained_label_path
+  # for test_data in 'test' 'train'; do # 'dev'
+  #   eval_masklm_data='/local/datdb/deepgo/data/train/fold_1/TokenClassify/TwoEmb/'$test_data'-'$ontology'-prot-annot.tsv'
+  #   CUDA_VISIBLE_DEVICES=3 python3 -u RunTokenClassifyProtData.py --cache_name $cache_name --block_size $block_size --mlm --bert_vocab $bert_vocab --train_data_file $train_masklm_data --output_dir $output_dir --per_gpu_eval_batch_size 2 --config_name $config_name --do_eval --model_type $model_type --overwrite_output_dir --evaluate_during_training --eval_data_file $eval_masklm_data --label_2test $label_2test --config_override --eval_all_checkpoints --checkpoint $checkpoint > $output_dir/'eval_'$test_data'_check_point_max.txt'
+  # done  # --pretrained_label_path $pretrained_label_path --aa_type_file $aa_type_file --reset_emb_zero
 
   ## view weights ??
   # cd $server/BertGOAnnotation/SeeAttention/
