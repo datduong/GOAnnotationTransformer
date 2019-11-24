7b1d0df601797205b2753214695b1d03c160d38f
Dat Duong <datd1988@gmail.com>
2019-11-24 04:02:11 -0800

    up script

--- finetune/RenameFolder.sh
+++ finetune/RenameFolder.sh
@@ -1,18 +1,18 @@
 
 for onto in mf cc bp ; do 
-cd /local/datdb/deepgo/data/BertNotFtAARawSeqGO/$onto/fold_1/2embPpiAnnotE256H1L12I512Set0/ProtAnnotTypeTopo/YesPpiYesTypeScaleFreezeBert12Ep10e10Drop0.1
+cd /local/datdb/deepgo/data/BertNotFtAARawSeqGO/$onto/fold_1/2embPpiAnnotE256H1L12I512Set0/NoPpiYesTypeEp10e10Drop0.1
 mkdir ArchiveTrainWithMissingData/
 mv * ArchiveTrainWithMissingData/
 scp ArchiveTrainWithMissingData/vocab* ArchiveTrainWithMissingData/config.json .
 done 
 
 
-YesPpiYesTypeScaleFreezeElmoEp10e10Drop0
+YesPpiYesTypeScaleFreezeBert12Ep10e10Drop0
 
 
 for onto in mf cc bp ; do 
-  mkdir /local/datdb/deepgo/data/BertNotFtAARawSeqGO/$onto/fold_1/2embPpiAnnotE256H1L12I512Set0/YesPpiYesTypeScaleFreezeElmoEp10e10Drop0.1
-  cd /local/datdb/deepgo/data/BertNotFtAARawSeqGO/$onto/fold_1/2embPpiAnnotE256H1L12I512Set0/YesPpiYesTypeScaleFreezeElmoEp10e10Drop0.1
+  mkdir /local/datdb/deepgo/data/BertNotFtAARawSeqGO/$onto/fold_1/2embPpiAnnotE256H1L12I512Set0/NoPpiYesTypeScaleFreezeBert12Ep10e10Drop0.1
+  cd /local/datdb/deepgo/data/BertNotFtAARawSeqGO/$onto/fold_1/2embPpiAnnotE256H1L12I512Set0/NoPpiYesTypeScaleFreezeBert12Ep10e10Drop0.1
   scp /local/datdb/deepgo/data/BertNotFtAARawSeqGO/$onto/fold_1/2embPpiAnnotE256H1L12I512Set0/YesPpiYesTypeScaleFreezeBert12Ep10e10Drop0.1/vocab* .
   scp /local/datdb/deepgo/data/BertNotFtAARawSeqGO/$onto/fold_1/2embPpiAnnotE256H1L12I512Set0/YesPpiYesTypeScaleFreezeBert12Ep10e10Drop0.1/config.json .
 done 
