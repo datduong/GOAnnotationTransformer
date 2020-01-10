0b04e05233f010759ddbe8ca9d81c992f0b3aaf2
Dat Duong <datd1988@gmail.com>
2020-01-10 01:18:02 -0800

    clean up

--- SeeAttention/run.sh
+++ SeeAttention/run.sh
@@ -6,3 +6,10 @@ cd /u/scratch/d/datduong/BertGOAnnotation/SeeAttention
 Rscript plot_each_head.R 
 
 
+
+
+
+for onto in mf cc bp ; do
+  scp -r $hoffman:$scratch/deepgo/data/BertNotFtAARawSeqGO/$onto/fold_1/2embPpiAnnotE256H1L12I512Set0/No*Yes* /cygdrive/e/BertNotFtAARawSeqGO/$onto/fold_1/2embPpiAnnotE256H1L12I512Set0
+done 
+
