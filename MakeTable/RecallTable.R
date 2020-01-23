fc58204ab845c42bba1151c8cca8c2dd975dc7c4
Dat Duong <datd1988@gmail.com>
2020-01-22 16:59:52 -0800

    fix recall pasting wrong

--- MakeTable/RecallTable.R
+++ MakeTable/RecallTable.R
@@ -1,4 +1,8 @@
 
+library(taRifx)
+library(xtable)
+library(knitr)
+library(kableExtra)
 
 fin2 = read.table('GOannotationRecallJan21.txt',sep='\t',header=T,stringsAsFactors=F)
 fin2[,2:ncol(fin2)] = fin2[,2:ncol(fin2)] * 100
@@ -11,8 +15,11 @@ pack_rows("BLAST Psi-BLAST", 1, 2, label_row_css = "background-color: #666; colo
 pack_rows("DeepGO", 3, 4, label_row_css = "background-color: #666; color: #fff;") %>% 
 pack_rows("Transformer", 5, 8, label_row_css = "background-color: #666; color: #fff;")
 
-
-
+#### recall rate on rare labels for LARGER DATA.
+library(taRifx)
+library(xtable)
+library(knitr)
+library(kableExtra)
 fin2 = read.table('GOannotationRecallJan21Large.txt',sep='\t',header=T,stringsAsFactors=F)
 fin2[,2:ncol(fin2)] = fin2[,2:ncol(fin2)] * 100
 # fin2$Row = 1:nrow(fin2)
@@ -22,6 +29,6 @@ add_header_above(c(" "=2, "BP"=3, "MF"=3, "CC"=3)) %>%
 kable_styling(latex_options = c("hold_position","scale_down"), position = "center") %>%
 pack_rows("BLAST Psi-BLAST", 1, 2, label_row_css = "background-color: #666; color: #fff;") %>% 
 pack_rows("DeepGO", 3, 4, label_row_css = "background-color: #666; color: #fff;") %>% 
-pack_rows("Transformer", 5, 7, label_row_css = "background-color: #666; color: #fff;")
+pack_rows("Transformer", 5, 8, label_row_css = "background-color: #666; color: #fff;")
 
 
