9585dffed55acae6d57714790e6c8d06e57a4e48
Dat Duong <datd1988@gmail.com>
2020-07-11 23:03:10 -0700

    make table

--- MakeTable/RecallTable.R
+++ MakeTable/RecallTable.R
@@ -42,16 +42,39 @@ library(taRifx)
 library(xtable)
 library(knitr)
 library(kableExtra)
-# fin2 = read.table('GOannotationRecallJul11LargeRareLabel.txt',sep='\t',header=T,stringsAsFactors=F)
-fin2 = read.table('GOannotationRecallJul11LargeCommonLabel.txt',header=T,stringsAsFactors=F)
+fin2 = read.table('GOannotationRecallJul11LargeRareLabel.txt',header=T,stringsAsFactors=F)
+# fin2 = read.table('GOannotationRecallJul11LargeCommonLabel.txt',header=T,stringsAsFactors=F)
 fin2[,2:ncol(fin2)] = fin2[,2:ncol(fin2)] # * 100
 # fin2$Row = 1:nrow(fin2)
 # colnames(fin2)=NULL
 kable( fin2, "latex", longtable = F, booktabs = T, caption = "blank", row.names=TRUE ) %>% #row.names=c(1:nrow(fin2))
+add_header_above(c(" "=2, "Macro"=1, "R@k"=3, "Macro"=1, "R@k"=3, "Macro"=1, "R@k"=3)) %>%
 add_header_above(c(" "=2, "BP"=4, "MF"=4, "CC"=4)) %>%
 kable_styling(latex_options = c("hold_position","scale_down"), position = "center") %>%
 pack_rows("BLAST Psi-BLAST", 1, 2, label_row_css = "background-color: #666; color: #fff;") %>% 
 pack_rows("DeepGO", 3, 4, label_row_css = "background-color: #666; color: #fff;") %>% 
-pack_rows("DeepGOPlus", 5, 6, label_row_css = "background-color: #666; color: #fff;") %>% 
-pack_rows("Transformer", 7, 9, label_row_css = "background-color: #666; color: #fff;")
+pack_rows("DeepGOPlus", 5, 5, label_row_css = "background-color: #666; color: #fff;") %>% 
+pack_rows("GOAT", 6, 10, label_row_css = "background-color: #666; color: #fff;")
+
+
+
+
+
+#! recall rate on rare labels for ORIGINAL DATA
+library(taRifx)
+library(xtable)
+library(knitr)
+library(kableExtra)
+# fin2 = read.table('GOannotationRecallJul11LargeRareLabel.txt',sep='\t',header=T,stringsAsFactors=F)
+fin2 = read.table('RecallOnDeepGOData25QuantileJuly11.txt',header=T,stringsAsFactors=F)
+fin2[,2:ncol(fin2)] = fin2[,2:ncol(fin2)] * 100
+# fin2$Row = 1:nrow(fin2)
+# colnames(fin2)=NULL
+kable( fin2, "latex", longtable = F, booktabs = T, caption = "blank", row.names=TRUE ) %>% #row.names=c(1:nrow(fin2))
+add_header_above(c(" "=2, "BP"=3, "MF"=3, "CC"=3)) %>%
+kable_styling(latex_options = c("hold_position","scale_down"), position = "center") %>%
+pack_rows("BLAST Psi-BLAST", 1, 2, label_row_css = "background-color: #666; color: #fff;") %>% 
+pack_rows("DeepGO", 3, 4, label_row_css = "background-color: #666; color: #fff;") %>% 
+pack_rows("Transformer", 5, 8, label_row_css = "background-color: #666; color: #fff;")
+
 
